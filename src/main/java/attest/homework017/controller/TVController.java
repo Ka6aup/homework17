a2ab8029716ba26ad417e6d49a05b3e209116264
Dinar S <ka6aup@gmail.com>
2024-10-15 17:12:14 +0300

    Hw19-20

--- src/main/java/attest/homework017/controller/TVController.java
+++ src/main/java/attest/homework017/controller/TVController.java
@@ -2,6 +2,8 @@ package attest.homework017.controller;
 
 import attest.homework017.model.TV;
 import attest.homework017.service.TVService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tv")
+@Tag(name = "TV")
 public class TVController {
 
     private final TVService tvService;
@@ -21,12 +24,14 @@ public class TVController {
     }
 
     @PostMapping
+    @Operation(summary = "Создание new TV")
     public ResponseEntity<TV> createTV(@RequestBody TV tv) {
         TV createdTV = tvService.createTV(tv);
         return new ResponseEntity<>(createdTV, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Обновление info TV")
     public ResponseEntity<TV> updateTV(@PathVariable Long id, @RequestBody TV tv) {
         tv.setId(id);
         TV updatedTV = tvService.updateTV(tv);
@@ -34,18 +39,22 @@ public class TVController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Удаление TV")
     public ResponseEntity<Void> deleteTV(@PathVariable Long id) {
         tvService.deleteTV(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Получение info TV по ID")
+
     public ResponseEntity<TV> getTVById(@PathVariable Long id) {
         TV tv = tvService.getTVById(id);
         return ResponseEntity.ok(tv);
     }
 
     @GetMapping
+    @Operation(summary = "Получеине списка всех TV")
     public ResponseEntity<List<TV>> getAllTVs() {
         List<TV> tvs = tvService.getAllTVs();
         return ResponseEntity.ok(tvs);
